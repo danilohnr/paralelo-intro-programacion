64202a949019c0b25a90355dd3dad5ad981bce58
Danilo Noguera <danilo.noguera@gmail.com>
2022-08-09 15:02:31 -0600

    1-Creacion programa par-impar

---
+++ src/programasejemplo/ParImpar.java
@@ -0,0 +1,22 @@
+package programasejemplo;
+
+import java.util.Scanner;
+
+public class ParImpar {
+
+	public static void main(String[] args) {
+		System.out.println("Determinar si un número es par o impar");
+		System.out.print("Ingrese el número: ");
+		Scanner entrada = new Scanner(System.in);
+		int num = entrada.nextInt();
+		if (num <= 0) {
+			System.out.println("Número inválido: " + num);
+		} else if (num %2 == 0){
+			System.out.println(num + " es PAR");
+		} else {
+			System.out.println(num + " es IMPAR");
+		}
+		entrada.close();
+	}
+
+}

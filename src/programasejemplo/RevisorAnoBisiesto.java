4b12490331ddebfb04b5635cbb954e8766a6bb38
Danilo Noguera <danilo.noguera@gmail.com>
2022-08-09 15:34:10 -0600

    4-Agregar try en el programa revisor año bisiesto

--- src/programasejemplo/RevisorAnoBisiesto.java
+++ src/programasejemplo/RevisorAnoBisiesto.java
@@ -7,14 +7,15 @@ public class RevisorAnoBisiesto {
 	public static void main(String[] args) {
 		System.out.println("Dado un año, determinar si es bisiesto o no");
 		System.out.print("Ingrese el año: ");
-		Scanner entrada = new Scanner(System.in);
+			try(Scanner entrada = new Scanner(System.in)){
 		int anio = entrada.nextInt();
 		if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
 			System.out.println(anio + " es BISIESTO");
 		} else {
 			System.out.println(anio + " NO es BISIESTO");
 		}
-		entrada.close();
+		}
+		//entrada.close();
 	}
 
 }

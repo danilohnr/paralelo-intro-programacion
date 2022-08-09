e571e7abae0fd1eb32d095c2e01a25be93efb1af
Danilo Noguera <danilo.noguera@gmail.com>
2022-08-09 15:11:30 -0600

    3-Agregar throw IllegalArgumentException dentro del try en el programa par-impar

--- src/programasejemplo/ParImpar.java
+++ src/programasejemplo/ParImpar.java
@@ -10,12 +10,14 @@ public class ParImpar {
 		try (Scanner entrada = new Scanner(System.in);){
 			int num = entrada.nextInt();
 			if (num <= 0) {
-				System.out.println("Número inválido: " + num);
+				//System.out.println("Número inválido: " + num);
+				throw new IllegalArgumentException("Número inválido: " + num);
 			} else if (num %2 == 0){
 				System.out.println(num + " es PAR");
 			} else {
 				System.out.println(num + " es IMPAR");
 			}
+			//entrada.close();
 		}
 		
 	}

f2a696f213bdcff6857419a3f03e2ea7493241a4
Danilo Noguera <danilo.noguera@gmail.com>
2022-08-09 19:03:11 -0600

    8-Creacion programa imprime-digitos-de-numero

---
+++ src/programasejemplo/ImprimeDigitosDeNumero.java
@@ -0,0 +1,19 @@
+package programasejemplo;
+
+import java.util.Scanner;
+
+public class ImprimeDigitosDeNumero {
+
+	public static void main(String[] args) {
+		System.out.println("Dado un número entero, se extraerá e imprimirá sus dígitos");
+		System.out.print("Ingrese un numero: ");
+		Scanner entrada = new Scanner(System.in);
+		int digito, num = entrada.nextInt();
+		while (num != 0) {
+			digito = num % 10;
+			System.out.println(digito + " ");
+			num = num / 10;
+		}
+		entrada.close();
+	}
+}

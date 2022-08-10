c97214559bc5d69e82d13b76504e80d65aad552f
Danilo Noguera <danilo.noguera@gmail.com>
2022-08-09 19:07:03 -0600

    8-Se agregó try al programa imprime-digitos-de-numero

--- src/programasejemplo/ImprimeDigitosDeNumero.java
+++ src/programasejemplo/ImprimeDigitosDeNumero.java
@@ -7,13 +7,14 @@ public class ImprimeDigitosDeNumero {
 	public static void main(String[] args) {
 		System.out.println("Dado un número entero, se extraerá e imprimirá sus dígitos");
 		System.out.print("Ingrese un numero: ");
-		Scanner entrada = new Scanner(System.in);
-		int digito, num = entrada.nextInt();
-		while (num != 0) {
-			digito = num % 10;
-			System.out.print(digito + " ");
-			num = num / 10;
+		try(Scanner entrada = new Scanner(System.in)){
+			int digito, num = entrada.nextInt();
+			while (num != 0) {
+				digito = num % 10;
+				System.out.print(digito + " ");
+				num = num / 10;
+			}
+			//entrada.close();
 		}
-		entrada.close();
 	}
 }

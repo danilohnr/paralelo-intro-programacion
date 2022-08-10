a8575c85e515a569bc583a1a079b914b268d7a91
Danilo Noguera <danilo.noguera@gmail.com>
2022-08-09 18:33:41 -0600

    7-Se acomodó los {} del try en el programa revisor año bisiesto

--- src/programasejemplo/RevisorAnoBisiesto.java
+++ src/programasejemplo/RevisorAnoBisiesto.java
@@ -8,13 +8,13 @@ public class RevisorAnoBisiesto {
 		System.out.println("Dado un año, determinar si es bisiesto o no");
 		System.out.print("Ingrese el año: ");
 			try(Scanner entrada = new Scanner(System.in)){
-		int anio = entrada.nextInt();
-		if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
-			System.out.println(anio + " es BISIESTO");
-		} else {
-			System.out.println(anio + " NO es BISIESTO");
-		}
-		}
+				int anio = entrada.nextInt();
+				if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+					System.out.println(anio + " es BISIESTO");
+				} else {
+					System.out.println(anio + " NO es BISIESTO");
+				}
+			}
 		//entrada.close();
 	}
 

f96a1a547e2b95f5691f13fdf983f09a47d64d49
Danilo Noguera <danilo.noguera@gmail.com>
2022-08-15 13:35:19 -0600

    9- Se agregó el try al programa que imprime los dígitos de un número

--- src/programasejemplo/SumaDeDigitos.java
+++ src/programasejemplo/SumaDeDigitos.java
@@ -7,15 +7,16 @@ public class SumaDeDigitos {
 	public static void main(String[] args) {
 		System.out.println("Dato un número entero, extraer sus dígitos y sumarlos.");
 		System.out.print("Ingrese un número: ");
-		Scanner entrada = new Scanner(System.in);
-		int digito, suma = 0, num = entrada.nextInt();
-		while ( num != 0) {
-			digito = num % 10;
-			suma += digito;
-			num /= 10;
+		try(Scanner entrada = new Scanner(System.in)){
+			int digito, suma = 0, num = entrada.nextInt();
+			while ( num != 0) {
+				digito = num % 10;
+				suma += digito;
+				num /= 10;
+			}
+			System.out.println("La suma de los dígitos es " + suma);
+			//entrada.close();
 		}
-		System.out.println("La suma de los dígitos es " + suma);
-		entrada.close();
 	}
 
 }
